ac066fe87fd8a0977f745a8526424cbab5eacabb
krypek <krypek@tuta.io>
2024-06-16 17:46:38 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+D=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+if [ "$(whoami)" != 'root' ]; then
+    echo 'Script needs to be run as root.'
+    exit 1
+fi
+
+cd /boot/grub/themes/
+rm -rf crossgrub
+mkdir -p crossgrub
+cd crossgrub
+cp "$D"/*.png "$D"/theme.txt "$D"/*.pf2 .
+
+echo Installed to /boot/grub/themes
